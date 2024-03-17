77b961bfbaebe882fd5d87759a2781f7dafa93f6
Nina <ninarojanek@gmail.com>
2024-03-17 02:50:14 +0100

    day one completed

--- day1-printing.py
+++ day1-printing.py
@@ -1,17 +1,35 @@
 # a = "Welcome to the Band Name Generator"
-# b = input("What's name of the city you grew up in?\n")
-# c = input("What's your pet's name?\n")
-# result = b + " " + c
-# print("Your band name is:\n"+b+" "+c)
+# b = print("What's name of the city you grew up in?")
+# city = input()
+# pet = input("What's your pet's name?\n")
+# print("Your band name is:\n"+ city + " " + pet)
 
+#-----------------------------------
 # # Print Modifiers & String manipulation
 # print('She said: "Hello" and then left.')
 # print("She said: \"Hello\" and then left.")
 # print("What's your pet's name?")
 # print("Hello world!\nHello world!\nHello world") 
-
+#-----------------------------------
 # num1 = int(input())
 # num2 = int(input())
 # print(num1+num2)
 # print(num1*num2)
+#-----------------------------------
+# num_of_letters = len("Angela")
+# print(num_of_letters)
+#-----------------------------------
+# string = input("Input a string:\n")
+# lenght = len(string)
+# print("Length of string "+string+" is: "+ str(lenght))
+#-----------------------------------
+# a = input()
+# b = input()
 
+# c = a
+# a = b
+# b = c
+
+# print(a)
+# print(b)
+#-----------------------------------
---
+++ day2-data-types.py
@@ -0,0 +1,6 @@
+# print("Welcome to the tip calculator!)
+# bill = input("What was the total bill? $\n")
+# How much tip would you like to give? 10, 12 or 15?
+# How may people to split the bill? -5
+# Each person should pay: 
+
