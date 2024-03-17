f768e537978c161cb333f87af9fa6ba473c52e34
Nina <ninarojanek@gmail.com>
2024-03-17 02:25:40 +0100

    first day started

---
+++ day1-printing.py
@@ -0,0 +1,17 @@
+# a = "Welcome to the Band Name Generator"
+# b = input("What's name of the city you grew up in?\n")
+# c = input("What's your pet's name?\n")
+# result = b + " " + c
+# print("Your band name is:\n"+b+" "+c)
+
+# # Print Modifiers & String manipulation
+# print('She said: "Hello" and then left.')
+# print("She said: \"Hello\" and then left.")
+# print("What's your pet's name?")
+# print("Hello world!\nHello world!\nHello world") 
+
+# num1 = int(input())
+# num2 = int(input())
+# print(num1+num2)
+# print(num1*num2)
+
